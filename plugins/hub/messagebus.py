fc9cc8716f175c9f3dcb54d0a681723da44fdee7
Mike McLean <mikem@redhat.com>
2016-05-25 17:11:35 -0400

    defer messagebus sync

--- plugins/hub/messagebus.py
+++ plugins/hub/messagebus.py
@@ -240,7 +240,7 @@ def send_messages(cbtype, *args, **kws):
         return
     sender = get_sender()
     for message in messages:
-        sender.send(message, sync=True, timeout=config.getfloat('broker', 'timeout'))
+        sender.send(message, sync=False, timeout=config.getfloat('broker', 'timeout'))
     sender.close(timeout=config.getfloat('broker', 'timeout'))
 
     # koji should do this for us, but just in case...
